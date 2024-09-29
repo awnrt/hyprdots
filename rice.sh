2d6ebbe93d66106d7b39ff1a402e585e66a2efa0
awy <awy@awy.one>
2024-09-29 17:26:00 +0300

    gruvbox

--- rice.sh
+++ rice.sh
@@ -23,14 +23,14 @@ doas -u $PERMUSER mkdir -p /home/$PERMUSER/.local/share/themes
 doas -u $PERMUSER mkdir -p /home/$PERMUSER/.local/share/icons
 
 cd $WORKDIRECTORY
-doas -u $PERMUSER curl -LO "https://github.com/catppuccin/gtk/releases/download/v1.0.3/catppuccin-mocha-yellow-standard+default.zip"
-doas -u $PERMUSER unzip catppuccin-mocha-yellow-standard+default.zip
-doas -u $PERMUSER mv catppuccin-mocha-yellow-standard+default catppuccin-yellow
-doas -u $PERMUSER cp -r catppuccin-yellow /home/$PERMUSER/.local/share/themes
+doas -u $PERMUSER git clone https://github.com/awnrt/gruvbox-gtk-theme 
+doas -u $PERMUSER cp -r gruvbox-gtk-theme/Gruvbox-Dark /home/$PERMUSER/.local/share/themes
+doas -u $PERMUSER cp -r gruvbox-gtk-theme/Gruvbox-Icons /home/$PERMUSER/.local/share/icons
 
-doas -u $PERMUSER dbus-launch gsettings set org.gnome.desktop.interface gtk-theme "catppuccin-yellow"
+doas -u $PERMUSER dbus-launch gsettings set org.gnome.desktop.interface gtk-theme "Gruvbox-Dark"
+doas -u $PERMUSER dbus-launch gsettings set org.gnome.desktop.interface icon-theme "Gruvbox-Icons"
 doas -u $PERMUSER dbus-launch gsettings set org.gnome.desktop.wm.preferences button-layout 'appmenu'
-doas -u $PERMUSER dbus-launch gsettings set org.gnome.desktop.interface font-name "Ubuntu 11"
+doas -u $PERMUSER dbus-launch gsettings set org.gnome.desktop.interface font-name "LiterationSans Nerd Font 11"
 
 cd $WORKDIRECTORY
 doas -u $PERMUSER git clone https://github.com/zdharma-continuum/fast-syntax-highlighting
